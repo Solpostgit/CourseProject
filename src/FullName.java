f2ec097b7155ad351ba0c59b2d08a704744d1d6d
Solpostgit <begemot_off@mail.ru>
2023-09-11 20:03:28 +0300

    Added class FullName

--- src/FullName.java
+++ src/FullName.java
@@ -1,2 +1,27 @@
 public class FullName {
+    private String surname;
+    private String name;
+    private String patronymic;
+
+    public FullName(String surname, String name, String patronymic) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPatronymic() {
+        return this.patronymic;
+    }
+
+    public String toString() {
+        return " Фамилия " + this.surname + "Имя " + this.name + "Отчество " + this.patronymic;
+    }
 }
